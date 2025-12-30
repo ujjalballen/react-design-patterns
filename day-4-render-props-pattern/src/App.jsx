48f8e1098206a5ebb7a815a9086cb7172105e5c8
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-12-31 02:49:48 +0600

    implement render props pattern

--- day-4-render-props-pattern/src/App.jsx
+++ day-4-render-props-pattern/src/App.jsx
@@ -1,15 +1,32 @@
-import BikeTracker from "./components/messy/BikeTracker";
-import CarTracker from "./components/messy/CarTracker";
-
-
+// import BikeTracker from "./components/messy/BikeTracker";
+// import CarTracker from "./components/messy/CarTracker";
+import MouseTracker from "./solve-with-pattern/MouseTracker";
 
 function App() {
-    return (
-        <div className="flex flex-col items-center m-2">
-            <CarTracker />
-            <BikeTracker />
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center m-2">
+      {/* <CarTracker />
+            <BikeTracker /> */}
+
+      {/* car */}
+      <MouseTracker
+        render={(pos) => (
+          <p>
+            🚗 Car is at ({pos.x}, {pos.y})
+          </p>
+        )}
+      />
+
+      {/* bike */}
+      <MouseTracker
+        render={(pos) => (
+          <p>
+            🏍️ Bike is at ({pos.x}, {pos.y})
+          </p>
+        )}
+      />
+    </div>
+  );
 }
 
 export default App;
